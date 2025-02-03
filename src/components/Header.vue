611f118044a5c4ddc482c33653a1988c22656960
lounis59 <lounis.habrouc@gmail.com>
2025-02-03 14:56:21 +0100

    blob

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="header">
+    <h1>Bonjour</h1>
+</div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.header{
+    background: black;
+    width: 100vw;
+    height: max-content;
+}
+.header h1{
+    color: white;
+}
+</style>
